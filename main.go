d169bb3290e1022875de375f6808ed1e42777339
Neil Cowie <thecowster@hotmail.co.uk>
2025-03-08 14:55:53 +0000

    hands on exercise 31 - for loop condition

--- main.go
+++ main.go
@@ -1,39 +1,20 @@
 /*
-Hands-on exercise #30
-	● Create one random int between 0 inclusive and 5 exclusive
-		○ assign the value to a variable with the identifier x
-	● Use a switch statement to print a description of the variable and value
-	● run the code 42 times and print the iteration number
+Hands-on exercise #31
+	● create a for loop using only a condition
+	● increment or decrement the variable being checked in the condition
 */
 
 package main
 
-import (
-	"fmt"
-
-	"math/rand"
-)
+import "fmt"
 
 func main() {
 
-	const iterations = 42
-	for i := 0; i < iterations; i++ {
-		x := rand.Intn(5)
-		fmt.Printf("iteration :is %v \t", i)
-
-		switch x {
-		case 0:
-			fmt.Println("x is 0")
-		case 1:
-			fmt.Println("x is 1")
-		case 2:
-			fmt.Println("x is 2")
-		case 3:
-			fmt.Println("x is 3")
-		case 4:
-			fmt.Println("x is 4")
-		default:
-			fmt.Printf("x is out of bounds (%v)\n", x)
-		}
+	x := 37
+	for x < 42 {
+		fmt.Printf("x is %v\n", x)
+		x++
 	}
+
+	fmt.Println("Done!")
 }
