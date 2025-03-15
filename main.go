df6a9bb05a83e477084201c4abf56da252ede49d
Neil Cowie <thecowster@hotmail.co.uk>
2025-03-15 09:45:20 +0000

    Hands on exercise 38 - statement statement idiom

--- main.go
+++ main.go
@@ -1,33 +1,27 @@
 /*
-Hands-on exercise #37
-	● use the code from the previous exercise
-	● add this code to print a single value stored in the map
-		age := m["James"]
-		fmt.Println(age)
-	● now use similar code to use the lookup of "Q" and print that value
-	● now use the "comma ok" idiom to test whether "Q" is actually stored in the map, then
-		print out a statement if it is not stored in the map
-		○ hint: check effective go for the "comma ok"*/
+Hands-on exercise #38
+	● use the "statement statement" idiom to
+		○ initialize x with and random int between 0 inclusive and 5 exclusive
+		○ if x is 3
+	■ print "x is 3"
+	● run that code 100 times
+	● what's the benefit of using the "statement statement" idiom?
+*/
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 
-	m := map[string]int{
-		"James":      42,
-		"Moneypenny": 32,
-	}
-
-	age := m["James"]
-	fmt.Println(age)
-
-	if age, ok := m["Q"]; !ok {
-		fmt.Println("cannot find Q in map, and the zero value of int is ", age)
-	}
-
-	if age, ok := m["James"]; ok {
-		fmt.Println("James Bond age is ", age)
+	c := 1
+	for i := 0; i < 100; i++ {
+		if x := rand.Intn(5); x == 3 {
+			fmt.Printf("iteration %v \t total count %v  \t x is %v\n", i, c, x)
+			c++
+		}
 	}
 }
